3bc7e543b5ce1536bfe88f1282240e9a0ee9571d
Matteo Tempesta <matteo.tempesta.91@gmail.com>
2015-12-01 17:12:07 +0100

    implementato un'esempio di uso di AJAX in UtenteView.jsp

--- WebContent/UtenteView.jsp
+++ WebContent/UtenteView.jsp
@@ -9,6 +9,35 @@
 </head>
 <body>
  <h1>Pannello Utente</h1>
+ <script src="http://code.jquery.com/jquery-latest.min.js"
+        type="text/javascript"></script>
+        <script>  
+$(document).ready(function(){   
+        alert ("attivazione richiesta");  
+          $.ajax({  
+                 url:'getSurvey',  
+                 success: function(response){  
+                     alert("risposta: " + response);
+                     for(var key in response) {
+                    	    var value = response[key];
+                    	    alert("value: " + value);
+                    	}
+                     
+                 }  
+});  
+});  
+
+</script> 
+ 
+ 
+ 
+ 
+ 
+ <!-- 
+ <s:action name="getSurvey" executeResult="true">
+ 	<s:property value="myBeanProperty" />
+ </s:action>
+ -->
  
 <div>
     <h3>Vuoi diventare Amministratore?</h3>
--- src/com/struts2/progettoSondaggio/UtenteAction.java
+++ src/com/struts2/progettoSondaggio/UtenteAction.java
@@ -1,8 +1,20 @@
 package com.struts2.progettoSondaggio;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class UtenteAction extends ActionSupport{
+	static final String url = "jdbc:mysql://localhost:3306/sondaggioDB";
+	static final String user = "root";
+	static final String psw = user;
+	public String Prova; 
+
 	
 	private PagamentoController pagamentoController;
 	
@@ -13,8 +25,58 @@ public class UtenteAction extends ActionSupport{
 		pagamentoController = new PagamentoController();
 	}
 	
+	
+	public String getSurvey() throws Exception {	
+		
+		this.Prova="cacca";
+		System.out.println("adaaad");
+		this.requestSurvey();
+		return SUCCESS;
+	}
+	
+	
+	public boolean requestSurvey()
+	{
+		System.out.println("ijijijijij");
+		
+		// esegue la query per ottenere i sondaggi da far apparire nell'utenteView
+	      try {
+	    	  System.out.println("ijijijijij");
+	          Class.forName("com.mysql.jdbc.Driver").newInstance();
+	          Connection con = DriverManager.getConnection(url, user, psw);
+	          Statement stmt = con.createStatement();
+
+	          ResultSet result = stmt.executeQuery("SELECT * FROM sondaggioDB.Sondaggio;");
+	          if (!result.isBeforeFirst() ) 
+	          {    
+	        	  return false;
+	          } 
+	          result.next();
+	          System.out.println("Braaaa: " + result.getString("sondaggioID"));
+	          if(Integer.parseInt(result.getString("sondaggioID"))>0)
+	          {
+	        	  System.out.println("sadasfadf");
+	        	  con.close();
+	        	  return true;
+	          }
+	          else 
+	          {
+	        	  con.close(); 
+	        	  return false;
+	          }
+
+	      } catch (Exception ex) {
+	          Logger.getLogger(LoginController.class.getName()).log( 
+	                           Level.SEVERE, null, ex);
+	      }
+	      return false;
+	}
+	
+	
+	/*
 	public String execute() throws Exception {	
 		
+		System.out.println("adaaad");
 		if(pagamentoController.effettuaPagamento(username))
 		{
 			System.out.println("pagamento true");
@@ -23,6 +85,8 @@ public class UtenteAction extends ActionSupport{
 		else return ERROR;
 		
 	}
+	*/
+	
 	
 	public String logout() throws Exception
 	{
@@ -36,5 +100,13 @@ public class UtenteAction extends ActionSupport{
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
+	 public String getProva() {
+	        return this.Prova;
+	    }
+	 
+	    public void setProva(String prova) {
+	        this.Prova = prova;
+	    }
 
 }
